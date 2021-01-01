85878c234047f314b4c9f6022397bc2f1af0a848
Riley Avron <ravron@posteo.net>
2021-01-01 08:58:43 -0800

    cat-file understands blobs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[macro_use]
+extern crate anyhow;
+
+mod cat_file;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct CatFile {
+    object_name: String,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "the stupid content tracker")]
+enum Git {
+    // #[structopt(flatten)]
+    CatFile(CatFile),
+    // CatFile {
+    //     object_name: String,
+    // },
+}
+
 fn main() {
     println!("Hello, world!");
+    let g: Git = Git::from_args();
+    println!("{:?}", g);
+    let result = match g {
+        Git::CatFile(cf) => cat_file::cat_file(&cf),
+    };
+    match result {
+        Ok(obj) => println!("{:?}", obj),
+        Err(e) => panic!("{}", e),
+    }
 }
